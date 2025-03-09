df9aac06c43b2d356178c3b1246f2c428cd7b63b
IvanZemsky <i.ivanzemsky2005@gmail.com>
2025-03-09 12:02:59 +0300

    feat: UiSelect story

--- src/components/UiSelect/UiSelect.stories.ts
+++ src/components/UiSelect/UiSelect.stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/vue3";
 import UiSelect from "./UiSelect.vue";
+import UiSelectItem from "./UiSelectItem.vue";
 
 const meta: Meta = {
    title: "Inputs/Select",
@@ -11,11 +12,19 @@ type Story = StoryObj<typeof UiSelect>
 
 export const Default: Story = {
    render: (args) => ({
-      components: { UiSelect },
+      components: { UiSelect, UiSelectItem },
       setup() {
          return { args }
       },
-      template: `<UiSelect />`,
+      template: `
+         <div style="height: 200px">
+            <ui-select placeholder="Select an option" style="width: 300px">
+               <ui-select-item value="option-1">Option 1</ui-select-item>
+               <ui-select-item value="option-2">Option 2</ui-select-item>
+               <ui-select-item value="option-3">Option 3</ui-select-item>
+            </ui-select>
+         </div>
+      `,
    })
 }
 
