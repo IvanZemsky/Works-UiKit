7f3b5de275059ccf0541c2f5711e5bbbccab57ec
IvanZemsky <i.ivanzemsky2005@gmail.com>
2025-05-08 00:05:34 +0300

    Updates

--- assets/UiButton.stories-CsnQ0eri.js
+++ assets/UiButton.stories-CsnQ0eri.js
@@ -1,4 +1,4 @@
-import{_ as n}from"./UiButton-C5G69dxR.js";import{S}from"./SearchOutlined-CtsSTKLU.js";import"./vue.esm-bundler-DRdyH_Db.js";import"./vue-router-BbbCpkhz.js";const z={component:n,title:"Inputs/Button",tags:["autodocs"]},e={render:t=>({components:{UiButton:n},setup(){return{args:t}},template:`
+import{_ as n}from"./UiButton-DxcEY9_T.js";import{S as x}from"./SearchOutlined-CtsSTKLU.js";import"./vue.esm-bundler-DRdyH_Db.js";import"./vue-router-BbbCpkhz.js";const z={component:n,title:"Inputs/Button",tags:["autodocs"]},e={render:t=>({components:{UiButton:n},setup(){return{args:t}},template:`
          <div style="display: flex; align-items: center; flex-wrap: wrap; gap: 1rem ">
             <ui-button>Button</ui-button>
             <ui-button variant="outlined">Button</ui-button>
@@ -9,7 +9,7 @@ import{_ as n}from"./UiButton-C5G69dxR.js";import{S}from"./SearchOutlined-CtsSTK
             <ui-button size="sm">Button</ui-button>
             <ui-button size="md">Button</ui-button>
             <ui-button size="lg">Button</ui-button>
-         </div>`})},u={render:t=>({components:{UiButton:n,SearchOutlined:S},setup(){return{args:t}},template:`
+         </div>`})},u={render:t=>({components:{UiButton:n,SearchOutlined:x},setup(){return{args:t}},template:`
          <div style="display: flex; align-items: center; flex-wrap: wrap; gap: 1rem ">
             <ui-button>
                <template #start-icon><SearchOutlined /></template>Start icon
@@ -26,6 +26,15 @@ import{_ as n}from"./UiButton-C5G69dxR.js";import{S}from"./SearchOutlined-CtsSTK
                <template #end-icon><SearchOutlined /></template>
             </ui-button>
             <ui-button>
+               <SearchOutlined />
+            </ui-button>
+            <ui-button variant="outlined">
+               <SearchOutlined />
+            </ui-button>
+            <ui-button variant="ghost">
+               <SearchOutlined />
+            </ui-button>
+            <ui-button variant="light">
                <SearchOutlined />
             </ui-button>
          </div>`})},i={render:t=>({components:{UiButton:n},setup(){return{args:t}},template:`
@@ -62,7 +71,7 @@ import{_ as n}from"./UiButton-C5G69dxR.js";import{S}from"./SearchOutlined-CtsSTK
             <ui-button variant="light">Button</ui-button>
          </div>\`
   })
-}`,...(l=(s=e.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};var p,c,m;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+}`,...(l=(s=e.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};var p,c,b;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
   render: args => ({
     components: {
       UiButton
@@ -79,7 +88,7 @@ import{_ as n}from"./UiButton-C5G69dxR.js";import{S}from"./SearchOutlined-CtsSTK
             <ui-button size="lg">Button</ui-button>
          </div>\`
   })
-}`,...(m=(c=o.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var d,b,g;u.parameters={...u.parameters,docs:{...(d=u.parameters)==null?void 0:d.docs,source:{originalSource:`{
+}`,...(b=(c=o.parameters)==null?void 0:c.docs)==null?void 0:b.source}}};var d,m,g;u.parameters={...u.parameters,docs:{...(d=u.parameters)==null?void 0:d.docs,source:{originalSource:`{
   render: args => ({
     components: {
       UiButton,
@@ -109,9 +118,18 @@ import{_ as n}from"./UiButton-C5G69dxR.js";import{S}from"./SearchOutlined-CtsSTK
             <ui-button>
                <SearchOutlined />
             </ui-button>
+            <ui-button variant="outlined">
+               <SearchOutlined />
+            </ui-button>
+            <ui-button variant="ghost">
+               <SearchOutlined />
+            </ui-button>
+            <ui-button variant="light">
+               <SearchOutlined />
+            </ui-button>
          </div>\`
   })
-}`,...(g=(b=u.parameters)==null?void 0:b.docs)==null?void 0:g.source}}};var v,B,f;i.parameters={...i.parameters,docs:{...(v=i.parameters)==null?void 0:v.docs,source:{originalSource:`{
+}`,...(g=(m=u.parameters)==null?void 0:m.docs)==null?void 0:g.source}}};var v,h,B;i.parameters={...i.parameters,docs:{...(v=i.parameters)==null?void 0:v.docs,source:{originalSource:`{
   render: args => ({
     components: {
       UiButton
@@ -126,7 +144,7 @@ import{_ as n}from"./UiButton-C5G69dxR.js";import{S}from"./SearchOutlined-CtsSTK
             Link to the official Storybook web site
          </ui-button>\`
   })
-}`,...(f=(B=i.parameters)==null?void 0:B.docs)==null?void 0:f.source}}};var h,y,x;a.parameters={...a.parameters,docs:{...(h=a.parameters)==null?void 0:h.docs,source:{originalSource:`{
+}`,...(B=(h=i.parameters)==null?void 0:h.docs)==null?void 0:B.source}}};var f,S,y;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:`{
   render: args => ({
     components: {
       UiButton
@@ -151,4 +169,4 @@ import{_ as n}from"./UiButton-C5G69dxR.js";import{S}from"./SearchOutlined-CtsSTK
          </div>
       \`
   })
-}`,...(x=(y=a.parameters)==null?void 0:y.docs)==null?void 0:x.source}}};const _=["Variant","Sizes","Icons","As","Colors"];export{i as As,a as Colors,u as Icons,o as Sizes,e as Variant,_ as __namedExportsOrder,z as default};
+}`,...(y=(S=a.parameters)==null?void 0:S.docs)==null?void 0:y.source}}};const _=["Variant","Sizes","Icons","As","Colors"];export{i as As,a as Colors,u as Icons,o as Sizes,e as Variant,_ as __namedExportsOrder,z as default};
